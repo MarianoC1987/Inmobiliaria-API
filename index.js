9b06fb584bae49dfbae07be918be2576e075d784
Mariano Camacho <macadema@gmail.com>
2023-01-06 18:13:21 -0300

    agregando 'module.exports = app' al final del index y mejorando cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 
 //RUTAS
 const users = require("./routes/users");
@@ -25,3 +25,5 @@ const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+module.exports = app;
